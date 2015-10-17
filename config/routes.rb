70f264504da7df4c319553831252ba77cdfe73dc
Eriko Verissimo <erikopa@gmail.com>
2015-10-17 18:30:51 -0300

    Configuração de rota educadores

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   post   'login'  => 'sessions#create'
   
   delete 'logout' => 'sessions#destroy'
+  
+  get 'educadores' => 'educators#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
